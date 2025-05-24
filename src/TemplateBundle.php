e8c0b37255a9da16d8e183c3363275cfd3a73cf3
Jeremy <jeremy.libert@luxapps.lu>
2025-05-24 11:03:57 +0200

    Initialize

---
+++ src/TemplateBundle.php
@@ -0,0 +1,11 @@
+<?php
+
+declare(strict_types=1);
+
+namespace LibertJeremy\Symfony\TemplateBundle;
+
+use LibertJeremy\Symfony\ConfigHelpers\Bundle\AbstractBundle;
+
+final class TemplateBundle extends AbstractBundle
+{
+}
